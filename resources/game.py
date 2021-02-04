d5495b1f490c53241733bc35441fc5c15e06f568
WarriorThirteen <araj.jad@gmail.com>
2021-02-04 18:25:32 +0000

    Dots, no UI

--- resources/game.py
+++ resources/game.py
@@ -227,6 +227,8 @@ class game:
         self.DISPLAY_GEOMETRY = (DISPLAY_WIDTH, DISPLAY_HEIGHT)
         self.UPS = 50    # updates per second
 
+        self.sectors_configured = False
+
         self.MY_ID = str(gethostbyname(gethostname()))
 
         
@@ -237,8 +239,13 @@ class game:
             "mouse_mode"    : False,
             "player_colour" : "#4ef35a",
             "player_name"   : "Jad",
+
             "random_seed"   : random.randint(0, 10000),
-            "rnd_count"     : 0
+            "random_count"  : 0,
+
+            "sector_size"   : 200,
+            "dot_mass"      : 10,
+            "dot_radius"    : 10
         }
 
         WORLD_WIDTH = self.parameters["world_width"]
@@ -260,7 +267,6 @@ class game:
 
         else:
             self.running_flag = None
-
 
 
     def configure_game(self, config_pack):
@@ -286,14 +292,70 @@ class game:
 
 
         # Add dots
-        # TODO spawn dots
-        dot_list = dot_list
+
+        self.configure_sectors()
+
+        # Place each dot in appropriate sector
+        for dot in eval(dot_list):
+            self.place_dot(dot)
 
 
         # Merge sets of parameters
         self.parameters = self.parameters | eval(new_parameters)
 
         print("[GAME]:Game Configured")
+
+
+    def configure_sectors(self):
+        '''
+        Establish the sector dictionary for the map.
+        '''
+        # Determine width and height in terms of sectors
+        self.sectors_wide = self.parameters["world_width"] // self.parameters["sector_size"] + 1
+        self.sectors_high = self.parameters["world_height"] // self.parameters["sector_size"] + 1
+
+        # Create sector dictionary
+        self.sectors = {}
+        
+        for x in range(self.sectors_wide):
+            for y in range(self.sectors_high):
+                self.sectors[(x, y)] = []
+
+        self.sectors_configured = True
+        print(f"[GAME]:Configured {self.sectors_wide * self.sectors_high} sectors")
+
+
+    def spawn_dots(self, count=1):
+        '''
+        Randomly spawn a given number of dots, incrementing random count accordingly
+        '''
+        for i in range(count):
+            # Seed the randomness module
+            seed = self.parameters["random_seed"] + self.parameters["random_count"]
+            self.parameters["random_count"] += 1
+            random.seed(seed)
+
+            # Generate coordinates
+            x = random.randint(0, self.parameters["world_width"])
+            y = random.randint(0, self.parameters["world_height"])
+
+            self.place_dot((x, y))
+
+
+        print(f"[GAME]:Spawned {count} dots")
+
+
+    def place_dot(self, dot):
+        '''
+        Place a dot in its appropriate sector
+        '''
+        # Find relevant sector
+        sector_x = dot[0] // self.parameters["sector_size"]
+        sector_y = dot[1] // self.parameters["sector_size"]
+
+        # Put dot in sector
+        self.sectors[(sector_x, sector_y)].append(dot)
+
 
 
     def create_blob(self, controller, controller_type="networked"):
@@ -311,6 +373,46 @@ class game:
         del self.blobs[controller]
 
 
+    def check_blob_eat(self, blob):
+        '''
+        Look around a blob and see if it has eaten any nearby dots.
+        If so, delete the dot and add mass to the blob
+        '''
+        # Find blob sector coords
+        blob_x, blob_y = blob.pos
+        sector_x, sector_y = int(blob_x // self.parameters["sector_size"]), int(blob_y // self.parameters["sector_size"])
+
+        # Calculate blob size in terms of sectors, add one to allow blob overlapping sector boundaries
+        radius_in_sectors = blob.radius // self.parameters["sector_size"] + 1
+
+
+        # Determine max and min sector coords to check
+        # ! Make sure we don't exceed or undercut sector coords, i.e. reference negative coords
+        if (min_x := sector_x - radius_in_sectors) < 0:
+            min_x = 0
+        if (max_x := sector_x + radius_in_sectors) > self.sectors_wide:
+            max_x = self.sectors_wide
+
+        if (min_y := sector_y - radius_in_sectors) < 0:
+            min_y = 0
+        if (max_y := sector_y + radius_in_sectors) > self.sectors_high:
+            max_y = self.sectors_high
+
+        # Start looking
+        for x in range(min_x, max_x):
+            for y in range(min_y, max_y):
+
+                # Check every dot in each sector
+                for index, dot in enumerate(self.sectors[(x,y)]):
+
+
+                    # Eat the dot or display the dot?
+                    if math.dist((blob_x, blob_y), dot) < blob.radius:
+                        # We are close enough to eat the dot
+                        blob.add_mass(self.parameters["dot_mass"])
+                        del self.sectors[(x,y)][index]
+
+
     def info_for_new(self):
         '''
         Return array of info to send to a new player
@@ -323,20 +425,20 @@ class game:
         '''
 
         # first part is our blobs and a string to recreate them
-        blobs_out = "" #"{"
+        blobs_out = ""
         for i in self.blobs:
             blobs_out += f"{i}:{str(self.blobs[i])},"
-        # blobs_out += "}"
 
 
         # now append the dot_list
-        # TODO
-        dots_out = "wee"
+        # This returns list of coords of every dot
+        # ! It just works don't touch
+        dots_out = str([a for sector in self.sectors for a in self.sectors[sector]])
 
 
         # now add parameters
         # These are the parameters we need to share with new player
-        mp_params = ("world_width", "world_height", "random_seed", "rnd_count")
+        mp_params = ("world_width", "world_height", "random_seed", "random_count")
 
         params_out = {i:self.parameters[i] for i in mp_params}
         params_out = str(params_out)
@@ -366,7 +468,7 @@ class game:
         '''
         # Transform data
         blob, mass, pos_x, pos_y = data.split(",")
-        pos = (int(pos_x), int(pos_y))
+        pos = (int(float(pos_x)), int(float(pos_y)))
         mass = int(mass)
 
         # Use data to relocate blob
@@ -388,9 +490,15 @@ class game:
         WORLD_HEIGHT = self.parameters["world_height"]
         self.WORLD_GEOMETRY = (WORLD_WIDTH, WORLD_HEIGHT)
 
+        # Find width and height of screen in terms of sectors, rounded up
+        # These are halved because we look from player's position in the centre of the screen
+        # Add one to make sure dots are rendered at the edge of the screen
+        screen_sector_width = (WORLD_WIDTH // self.parameters["sector_size"] + 1) // 2 + 1
+        screen_sector_height = (WORLD_HEIGHT // self.parameters["sector_size"] + 1) // 2 + 1
 
 
-        ##  configure display and pygame settings
+
+        # Configure display and pygame settings
         game_display = pygame.display.set_mode(self.DISPLAY_GEOMETRY)
         game_display.fill((250, 250, 250))
 
@@ -400,7 +508,7 @@ class game:
         clock = pygame.time.Clock()
 
 
-        ##  Creating things!
+        ## * Creating things!
 
 
 
@@ -424,6 +532,12 @@ class game:
         print(f"[GAME]:BLOBS LIST:{self.blobs}")
 
 
+        # If we aren't multiplayer, we haven't yet configured our sectors or spawned dots
+        if not self.sectors_configured:
+            self.configure_sectors()
+            self.spawn_dots(200)
+
+
         alive = True
         if self.running_flag != None:
             self.running_flag.set()
@@ -445,8 +559,6 @@ class game:
                 # print(f"[GAME]:{event}")
 
 
-
-
             # * Debugging
             pressed_keys = pygame.key.get_pressed()
 
@@ -457,13 +569,47 @@ class game:
                 player.add_mass(-10)
 
 
-
-
-
-            # Move things
+ 
+            # Move player
             player.move()
 
-            # display things
+
+            # * Eating time
+
+            for blob_name in self.blobs:
+                self.check_blob_eat(self.blobs[blob_name])
+
+
+            # * DISPLAY THINGS
+
+            # We need to render nearby dots
+            player_sector_x = int(player.pos[0] // self.parameters["sector_size"])
+            player_sector_y = int(player.pos[1] // self.parameters["sector_size"])
+
+            # Look across the screen from our position
+            # ! Make sure we don't exceed or undercut sector coords, i.e. reference negative coords
+            if (min_x := player_sector_x - screen_sector_width) < 0:
+                min_x = 0
+            if (max_x := player_sector_x + screen_sector_width) > self.sectors_wide:
+                max_x = self.sectors_wide
+
+            if (min_y := player_sector_y - screen_sector_height) < 0:
+                min_y = 0
+            if (max_y := player_sector_y + screen_sector_height) > self.sectors_high:
+                max_y = self.sectors_high
+
+            # Start looking
+            for x in range(min_x, max_x):
+                for y in range(min_y, max_y):
+
+                    # Check every dot in each sector
+                    for dot in self.sectors[(x,y)]:
+
+                        colour = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                        pygame.draw.circle(self.game_map, colour, dot, self.parameters["dot_radius"], self.parameters["dot_radius"])
+
+
+            # Display blobs
             for i in self.blobs:
                 self.blobs[i].display()
 
@@ -478,5 +624,5 @@ class game:
 if __name__ == "__main__":
 
     print("[GAME]:MAIN")
-    a = game()
-    a.run()
+    dummy_game = game()
+    dummy_game.run()
